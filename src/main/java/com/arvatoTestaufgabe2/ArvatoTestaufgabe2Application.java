d373783e80fa36c09ce6578546a3a8b98da76ef1
Boris H <bobo84@gmx.net>
2019-08-20 17:29:15 +0200

    application-properties definiert, dependencies in pom hinzugefügt

---
+++ src/main/java/com/arvatoTestaufgabe2/ArvatoTestaufgabe2Application.java
@@ -0,0 +1,13 @@
+package com.arvatoTestaufgabe2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArvatoTestaufgabe2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArvatoTestaufgabe2Application.class, args);
+	}
+
+}
